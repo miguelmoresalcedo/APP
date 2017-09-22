766b9418f3ce44eeaf6f204a5fd132a30350318d
miguelmoresalcedo <spyrone1@gmail.com>
2017-09-22 13:23:35 +0200

    Initial commit

---
+++ camara/www/camra.js
@@ -0,0 +1,77 @@
+var app = {
+	inicio: function(){
+		this.iniciaFastClick();
+		this.iniciaBoton();
+	},
+
+	iniciaFastClick: function() {
+		FastClick.attach(document.body);
+	},
+
+	iniciaBotones: function() {
+		var buttonAction = document.querySelector('#button-action');
+		buttonAction.addEventListener('click', this.tomarFoto);
+
+		var filterButtons = document.querySelectorAll('.button-filter');
+		filterButton[0].addEventListener('click', function(){
+			app.aplicaFiltro('gray');
+		});
+		filterButtons[1].addEventListener('click', function(){
+			app.aplicaFiltro('negative');
+		});
+		filterButtons[2].addEventListener('click', function(){
+			app.aplicaFiltro('sepia');
+		});
+
+
+	},
+
+	tomarFoto: function() {
+		var opciones = {
+			quality: 50,
+			destinationType: Camera.destinationType.File_URI,
+			targetWidth: 300,
+			targetHeight: 300,
+			correctOrientation: true
+		};
+		navigator.Camera.getPicture(app.fotoTomada, app.errorAlTomarFoto, opciones);
+	},
+
+	fotoTomada: function(imageURI){
+		var img = document.createElement('img');
+		img.onload = function(){
+			app.pintarFoto(img);
+		}
+		image.src = imageURI;
+	},
+
+	pintarFoto: function(img){
+		var canvas = document.querySelector('#foto');
+		var context = canvas.getContext('2d');
+		canvas.width = img.width;
+		canvas.height = img.height;
+		context.drawImage(img, 0, 0, img.width, img.height);
+	},
+
+	errorAlTomarFoto: function(message) {
+		console.log('Fallo al tomar foto o toma cancelada: ' + message); 
+
+	},
+
+	aplicaFiltro: function(filterName){
+		var canvas = document.querySelector('#foto');
+		var context = canvas.getContext('2d');
+		imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+		fabrik[filterName](imageData.data);
+		
+		context.putImageData(imageData, 0, 0);
+
+	}
+};
+
+if ('addEventListener' in document) {
+	document.addEventListener('DOMContentLoaded', function() {
+		app.inicio();
+	}, false);
+}
