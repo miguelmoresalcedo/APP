7020d762aa2bb8ab973637f3f7c44b5c5a344ba5
miguelmoresalcedo <spyrone1@gmail.com>
2017-10-13 11:19:47 +0200

    new
    project

--- 4. Sensores /Mio/juego.js
+++ 4. Sensores /Mio/juego.js
@@ -2,6 +2,7 @@ var app={
 	inicio:function(){
 	DIAMETRO_BOLA = 50;
 
+	dificultad=0;
 	velocidadX=0;
 	velocidadY=0;
 	puntuacion=0;
@@ -18,20 +19,29 @@ iniciaJuego: function(){
 		game.physics.starSystem(Phaser.Physics.ARCADE);
 
 		game.stage.backgroundColor = '#f27d0c';
-		game.load.image('bola', 'assets/bola.png'):
+		game.load.image('bola', 'bola.png');
+		game.load.image('nave', 'nave.png');
 	}
 	function create(){
 		scoreText = game.add.text(16, 16, puntuacion, { fontSize: '100px', fill: '#757676'});
-		bola = game.add.sprite(app.inicioX(), app.inicioY(), 'bola');
+		
+		nave = game.add.sprite(app.inicioX(), app.inicioY(), 'nave'); //lo pongo de manera aleatoria
+		bola = game.add.sprite(app.inicioX(), app.inicioY(), 'bola'); //lo pongo de manera aleatoria
+		
 		game.physics.arcade.enable(bola);
+		game.physics.arcade.enable(nave);
+
 		bola.body.collideWorldBounds = true;
 		bola.body.onWorldBounds = new Phaser.Signal();
 		bola.body.onWorldBounds.add(app.decrementaPuntuacion, this);
 	}
 
 	function update(){
-		bola.body.velocity.y = (velocidadY * 300);
-		bola.bosy.velocity.x = (velocidadX * -300);
+		var.factorDificultad = (300 + (dificultad * 100));
+		bola.body.velocity.y = (velocidadY * factorDificultad);
+		bola.bosy.velocity.x = (velocidadX * (-1 * factorDificultad));
+
+		game.physics.arcade.overlap(bola, objetivo, app.incrementaPuntuacion, null, this);
 	}
 
 	var estados = { preload: preload, create: create, update: update};
@@ -42,7 +52,19 @@ iniciaJuego: function(){
 decrementaPuntuacion: function(){
 	puntuacion = puntuacion -1;
 	scoreText.text = puntuacion;
-}
+},
+
+incrementaPuntuacion: function(){
+	puntuacion = puntuacion +1;
+	scoreText.text = puntuacion;
+
+	objetivo.body.x = app.inicioX();
+	objetivo.body.y = app.inicioY();
+
+	if(puntuacion > 0){
+		dificultad = dificultad +1
+	}
+},
 
 inicioX: function(){
 	return app.numeroAleatorioHasta(ancho - DIAMETRO_BOLA);
